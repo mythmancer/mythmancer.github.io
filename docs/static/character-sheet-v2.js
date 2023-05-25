abc8e45d538c669ce7482546e6f7448c191f3ec5
purajit <malalur2@illinois.edu>
2023-05-25 14:51:51 -0700

    Revert "[sheet v2] experiment with service workers for offline loading (#198)"
    This reverts commit fba0e6aef95b86f5ffa6a115af33f0e6184f98ff.

--- docs/static/character-sheet-v2.js
+++ docs/static/character-sheet-v2.js
@@ -2490,20 +2490,6 @@ function renderPage(characterName) {
 }
 
 function postRender() {
-  // setup service worker for offline loading, if available in browser
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/static/service-worker.js").then(
-      registration => {
-        console.log("Service worker registered");
-      },
-      error => {
-        console.log(`Service worker registration failed: ${error}`);
-      }
-    );
-  } else {
-    console.log(`Browser does not support service workers`);
-  }
-
   // all logic that needs to be done after rendering is done - such as attaching listeners
   attachComponentListeners();
   initializeComponents();
